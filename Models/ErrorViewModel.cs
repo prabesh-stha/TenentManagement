3110e7a8cd2020ba0d4da1b0b4c10a03fde5a865
prabesh-stha <shresthaprabesh155@gmail.com>
2025-06-26 18:21:25 +0545

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TenentManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
